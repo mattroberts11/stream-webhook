0f1f0389b02320dd0a66375f2a4046d2a0c81bbc
Matt Roberts <matt.roberts@getstream.io>
2021-11-19 13:32:33 -0700

    server update

--- app.js
+++ app.js
@@ -16,52 +16,52 @@ const serverClient = StreamChat.getInstance(appKey, secret);
 app.use(cors());
 app.use(express.json());
 
-app.get('/server', (req, res) => {
+app.get('/', (req, res) => {
     // Server running test
     res.send(`Hola Mundo!`);
 })
 
-app.post('/token', async (req, res) => {
-    console.log('REQUEST', req.body);
-    // get the userId from the front end body
-    const { userId } = req.body;
+// app.post('/token', async (req, res) => {
+//     console.log('REQUEST', req.body);
+//     // get the userId from the front end body
+//     const { userId } = req.body;
 
-    const token = serverClient.createToken(
-        userId,
-        // Math.floor(Date.now() / 1000) + (60 * 60) // expires in 1 hr
-    );
+//     const token = serverClient.createToken(
+//         userId,
+//         // Math.floor(Date.now() / 1000) + (60 * 60) // expires in 1 hr
+//     );
     
-    try {
-        res.status(200).send(token);
-    } catch (err) {
-        res.status(500).send("Error getting token", err);
-    }
-})
+//     try {
+//         res.status(200).send(token);
+//     } catch (err) {
+//         res.status(500).send("Error getting token", err);
+//     }
+// })
 
-app.post('/delete-user', (req, res) => {
+// app.post('/delete-user', (req, res) => {
 
-  console.log("delete user request", req.body);
+//   console.log("delete user request", req.body);
   
-  const { userId } = req.body;
+//   const { userId } = req.body;
 
-  const destroyUser = async (userId) => {
-    return await serverClient.deleteUser(userId, {
-      delete_conversation_channels: true, 
-      mark_messages_deleted: true, 
-      hard_delete: true,
-    })
-  }
+//   const destroyUser = async (userId) => {
+//     return await serverClient.deleteUser(userId, {
+//       delete_conversation_channels: true, 
+//       mark_messages_deleted: true, 
+//       hard_delete: true,
+//     })
+//   }
 
-  try {
-    destroyUser(userId)
-      // .then( response => console.log("DELETE USER RESPONSE", response))
-      .then( response => res.status(200).send(response))
+//   try {
+//     destroyUser(userId)
+//       // .then( response => console.log("DELETE USER RESPONSE", response))
+//       .then( response => res.status(200).send(response))
       
-  } catch (err) {
-    res.status(500).send('Error deleting user', err)
-  }
+//   } catch (err) {
+//     res.status(500).send('Error deleting user', err)
+//   }
 
-})
+// })
 
 app.listen(port, () => {
     console.log(`webhooks app listening on port ${port}`);
